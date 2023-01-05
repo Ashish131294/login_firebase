436c30e51d03e6b14c5907cdd987f7d136bda008
Ashish131294 <ashish.kachhadiya1114@gmail.com>
2023-01-05 09:11:16 +0530

    Initial commit

--- lib/view_cloud.dart
+++ lib/view_cloud.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:login_firebase/realtime_cloud_insert.dart';
 
 class view_cloud extends StatefulWidget {
   const view_cloud({Key? key}) : super(key: key);
@@ -10,6 +11,9 @@ class view_cloud extends StatefulWidget {
 
 class _view_cloudState extends State<view_cloud> {
 
+  TextEditingController t1=TextEditingController();
+  TextEditingController t2=TextEditingController();
+  
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('Student').snapshots();
 
   @override
@@ -40,6 +44,20 @@ body: StreamBuilder<QuerySnapshot>(
                 .then((value) => print("User Deleted"))
                 .catchError((error) => print("Failed to delete user: $error"));
           }, icon: Icon(Icons.delete)),
+
+
+          leading: IconButton(onPressed: () {
+            CollectionReference users = FirebaseFirestore.instance.collection('Student');
+
+            users
+                .doc(document.id)
+                .update({'name': '${t1.text}','contact': '${t2.text}'})
+                .then((value) => print("User Updated"))
+                .catchError((error) => print("Failed to update user: $error"));
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return realtime_cloud_insert();
+            },));
+          }, icon: Icon(Icons.edit)),
         );
       }).toList(),
     );
--- lib/view_realtime.dart
+++ lib/view_realtime.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class view_realtime extends StatefulWidget {
@@ -8,10 +10,53 @@ class view_realtime extends StatefulWidget {
 }
 
 class _view_realtimeState extends State<view_realtime> {
+  List keylist = [];
+  List namelist = [];
+  List contactlist = [];
+
+  @override
+  void initState() {
+    DatabaseReference starCountRef = FirebaseDatabase.instance.ref('Student');
+    starCountRef.onValue.listen((DatabaseEvent event) {
+      Map data = event.snapshot.value as Map;
+      keylist.clear();
+      namelist.clear();
+      contactlist.clear();
+      data.forEach((key, value) {
+        print("$key=>$value");
+        keylist.add(key);
+        Map m = value;
+        namelist.add(m['name']);
+        contactlist.add(m['contact']);
+        setState(() {});
+      });
+      print(keylist);
+      print(namelist);
+      print(contactlist);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("view_realtime"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("view_realtime"),
+      ),
+      body: ListView.builder(itemCount: keylist.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("${namelist[index]}"),
+            subtitle: Text("${contactlist[index]}"),
+            trailing: IconButton(onPressed: () async {
+              DatabaseReference ref = FirebaseDatabase.instance.ref('Student').child(keylist[index]);
+             await ref.remove();
+             setState(() {
 
+             });
+            }, icon: Icon(Icons.delete)),
+          );
+        },
+      ),
     );
   }
 }
